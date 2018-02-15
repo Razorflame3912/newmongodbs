0dff221a05d43b57355dafe777c1a40f847fc71c
Mansour <melsharawy@stuy.edu>
2018-02-15 14:44:59 -0500

    classwork

---
+++ pokemongo.py
@@ -0,0 +1,15 @@
+import pymongo, json
+
+
+###CODE FOR UPLOADING DB DATA
+
+'''f = open('pokemon.json','rU')
+datastring = f.read()
+dic = json.loads(datastring)
+
+connection = pymongo.MongoClient('homer.stuy.edu')
+db = connection['LoneMan']
+collection = db['pokemon']
+
+collection.insert_many(dic['pokemon'])
+'''
